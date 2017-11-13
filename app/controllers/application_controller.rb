40439a1bb95f53e899cbc2236e50257b90fec5cc
Maffo97 <massimo.maffei@studenti.unitn.it>
2017-11-13 17:49:16 +0100

    first version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
     render html: "hello world"
-    render html: "ciao ho aggiunto anche questo"
-    render html: "e anche questo" #ehi
   end
 end
