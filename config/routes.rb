204df8a64f451f339b7cd3e8782d16a5b4dd5c45
TobaJR <andrea.tobaldi@studenti.unitn.it>
2017-11-15 15:08:39 +0100

    added bootstrap and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :clients 
-      resources :invoices
-=======
+
   root 'pages#home'
   resources :clients
   resources :invoices
->>>>>>> added bootstrap and css
+
 end
