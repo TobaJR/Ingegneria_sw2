d590a80f4a28696f0621aae21d5fa2250bcdebdf
Maffo97 <massimo.maffei@studenti.unitn.it>
2017-12-06 16:56:11 +0100

    Views datails

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,7 +5,7 @@ class Client < ApplicationRecord
               format: {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
               uniqueness: true,
               confirmation: true
-    validates_presence_of :name, :age, :email
+    validates_presence_of :name, :surname, :age, :email
 
     def total_hours
       th = 0;
